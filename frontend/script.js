389b3b89a17f7368e8d303d75c9d49347806587e
Srivathsava <srivathsava88@gmail.com>
2026-06-04 15:37:20 +0530

    Added frontend UI

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,31 @@
-async function research() {
+const API_URL =
+"https://multi-agent-research-assistant-4-6buc3idhb.vercel.app";
+
+async function generateReport(){
 
     const topic =
         document.getElementById("topic").value;
 
+    document.getElementById("loading")
+        .innerText = "Generating report...";
+
     const response =
-        await fetch("/api/index", {
-            method: "POST",
-            headers: {
-                "Content-Type":
-                "application/json"
+        await fetch(API_URL,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
             },
-            body: JSON.stringify({
-                topic
+            body:JSON.stringify({
+                topic:topic
             })
         });
 
     const data =
         await response.json();
 
-    document.getElementById(
-        "output"
-    ).innerText = data.report;
+    document.getElementById("loading")
+        .innerText = "";
+
+    document.getElementById("output")
+        .innerText = data.report;
 }
